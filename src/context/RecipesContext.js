4cf6d239c322a80a921412b98cc5096aa6376fcc
Santiago <santo1996.29@gmail.com>
2020-11-15 15:28:19 -0400

    feature: query API for recipes

--- src/context/RecipesContext.js
+++ src/context/RecipesContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import axios from 'axios'
 
 export const RecipesContext = createContext()
 
@@ -9,6 +10,19 @@ const RecipesProvider = ({ children }) => {
         ingredient: '',
         category: '',
     })
+
+    const { ingredient, category } = query
+
+    useEffect(() => {
+        if (Object.values(query).filter((v) => v.trim() !== '').length === 2) {
+            const queryAPI = async () => {
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}c=${category}`
+                const payload = await axios.get(url)
+
+                setRecipes(payload.data.drinks)
+            }
+        }
+    }, [query])
 
     return (
         <RecipesContext.Provider value={{ setQuery }}>
