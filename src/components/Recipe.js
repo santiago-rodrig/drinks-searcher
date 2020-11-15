4a73fce5cc77665b5aa71c40c5d1b2e5185bdb2a
Santiago <santo1996.29@gmail.com>
2020-11-15 16:11:31 -0400

    fix: code format

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,21 +4,27 @@ import { ModalContext } from '../context/ModalContext'
 const Recipe = ({ recipe }) => {
     const { setIdRecipe } = useContext(ModalContext)
 
-    <div className="col-12 col-md-4 mb-3">
-        <div className="card w-100">
-            <img
-                className="card-img-top"
-                src={recipe.strDrinkThumb}
-                alt={recipe.strDrink}
-            />
-            <h2 className="card-header">{recipe.strDrink}</h2>
-            <div className="card-body">
-                <button type="button" className="btn btn-block btn-primary" onClick={setIdRecipe(recipe.idDrink)}>
-                    Ver Receta
-                </button>
+    return (
+        <div className="col-12 col-md-4 mb-3">
+            <div className="card w-100">
+                <img
+                    className="card-img-top"
+                    src={recipe.strDrinkThumb}
+                    alt={recipe.strDrink}
+                />
+                <h2 className="card-header">{recipe.strDrink}</h2>
+                <div className="card-body">
+                    <button
+                        type="button"
+                        className="btn btn-block btn-primary"
+                        onClick={() => setIdRecipe(recipe.idDrink)}
+                    >
+                        Ver Receta
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
+    )
 }
 
 export default Recipe
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,14 +1,13 @@
 import React, { createContext, useEffect, useState } from 'react'
 import axios from 'axios'
 
-
 export const ModalContext = createContext()
 
 const ModalProvider = ({ children }) => {
     const [idRecipe, setIdRecipe] = useState(null)
 
     return (
-        <ModalContext.Provider value={{setIdRecipe}}>
+        <ModalContext.Provider value={{ setIdRecipe }}>
             {children}
         </ModalContext.Provider>
     )
