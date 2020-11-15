99130de83864fe7c1ff12208a2f677cc1974a8fe
Santiago <santo1996.29@gmail.com>
2020-11-15 11:54:53 -0400

    setup: initial commit and cleansing

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className="bg-secondary col-12">
+      <form>
+        <h1>Form component</h1>
+      </form>
+    </div>
+  )
+}
+
+export default Form
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <header className="bg-primary">
+    <h1>Busca Recetas de Bebidas</h1>
+  </header>
+)
+
+export default Header
