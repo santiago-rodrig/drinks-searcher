df85e1152b2b3ff22ced12a1a27d84e72b67eb56
Santiago <santo1996.29@gmail.com>
2020-11-15 12:28:34 -0400

    feature: Form component jsx return value

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,35 @@ import React from "react";
 
 const Form = () => {
   return (
-    <div className="bg-secondary col-12">
+    <div className="col-12 mt-4">
       <form>
-        <h1>Form component</h1>
+        <fieldset>
+          <legend>Busca bebidas por categoría o ingrediente</legend>
+        </fieldset>
+        <div className="row">
+          <div className="col-md-4">
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="Ingrediente"
+            />
+          </div>
+          <div className="col-md-4">
+            <select className="form-control" name="category" defaultValue="">
+              <option value="" disabled>
+                -- Selecciona una categoría --
+              </option>
+            </select>
+          </div>
+          <div className="col-md-4">
+            <input
+              type="submit"
+              className="btn btn-block btn-primary"
+              value="Buscar"
+            />
+          </div>
+        </div>
       </form>
     </div>
   );
