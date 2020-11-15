67639e2dd41f876d6b804550a78fe29aa54827c9
Santiago <santo1996.29@gmail.com>
2020-11-15 16:55:06 -0400

    feature: fetch cocktail from API for the modal

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -5,6 +5,19 @@ export const ModalContext = createContext()
 
 const ModalProvider = ({ children }) => {
     const [idRecipe, setIdRecipe] = useState(null)
+    const [recipe, setRecipe] = useState({})
+
+    useEffect(() => {
+        if (idRecipe) {
+            const getRecipeFromAPI = async () => {
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecipe}`
+                const payload = await axios.get(url)
+                setRecipe(payload.data.drinks[0])
+            }
+
+            getRecipeFromAPI()
+        }
+    }, [idRecipe])
 
     return (
         <ModalContext.Provider value={{ setIdRecipe }}>
