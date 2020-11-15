d3c34894629495185d65de1c18cd5c5b29e195ed
Santiago <santo1996.29@gmail.com>
2020-11-15 16:03:31 -0400

    feature: image and button for Recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,7 +3,17 @@ import React from 'react'
 const Recipe = ({ recipe }) => (
     <div className="col-12 col-md-4 mb-3">
         <div className="card w-100">
+            <img
+                className="card-img-top"
+                src={recipe.strDrinkThumb}
+                alt={recipe.strDrink}
+            />
             <h2 className="card-header">{recipe.strDrink}</h2>
+            <div className="card-body">
+                <button type="button" className="btn btn-block btn-primary">
+                    Ver Receta
+                </button>
+            </div>
         </div>
     </div>
 )
